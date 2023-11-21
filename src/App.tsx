986f91eae9098d1962e11850b4227af223a9e2f9
connieya <gunny6026@naver.com>
2023-11-21 16:25:30 +0900

    feat : SideBar 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Authentication from "./containers/Authentication";
 import Main from "./components/Main";
+import Router from "./Pages/Router/Router";
 
 function App() {
   return (
-    <div className='App'>
-      <Main />
-      <Authentication />
+    <div className="App">
+      <Router />
+      {/* <Main />
+      <Authentication /> */}
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import logo from "../../assets/logo.jpg";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img className="w-40" src={logo} alt="로고" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
