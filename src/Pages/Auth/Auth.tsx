a0ff779f0dfd04c9dd042caf17a256647aab4d67
connieya <gunny6026@naver.com>
2023-11-23 11:14:16 +0900

    refactor : 로그인 페이지 tailwind css , formik , yup 적용

--- src/Pages/Auth/Auth.tsx
+++ src/Pages/Auth/Auth.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import SignIn from "../../components/Rester/SignIn";
+import { useLocation } from "react-router-dom";
+import SignUp from "../../components/Rester/SignUp";
 
 const Auth = () => {
-  return <div></div>;
+  const location = useLocation();
+  return (
+    <div className="">
+      <div className="flex items-center justify-center h-[100vh] space-x-5">
+        <div className="relative hidden lg:block border">
+          <div className="h-[35.3rem] w-[23rem]">
+            <img
+              className="h-full w-full"
+              src="https://cdn.pixabay.com/photo/2018/11/13/22/01/instagram-3814080_640.png"
+              alt=""
+            />
+            <MobileWrapper></MobileWrapper>
+          </div>
+        </div>
+        <div className=" w-[23vw]">
+          {location.pathname === "/login" ? <SignIn /> : <SignUp />}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
+
+const MobileWrapper = styled.div`
+  background-image: url("");
+`;
--- src/components/Rester/SignIn.tsx
+++ src/components/Rester/SignIn.tsx
@@ -1,7 +1,116 @@
 import React from "react";
+import logo from "../../assets/logo.jpg";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  Input,
+} from "@chakra-ui/react";
+import { Formik, Field, Form, FieldProps } from "formik";
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
+
+interface FormValues {
+  email: string;
+  password: string;
+}
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("잘못된 이메일 형식입니다.")
+    .required("이메일을 입력해주세요."),
+  password: Yup.string()
+    .min(4, "비밀번호는 최소 8자리 이상 입력해야 합니다.")
+    .required("비밀번호를 입력해주세요."),
+});
 
 const SignIn = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const handleSubmit = (values: FormValues) => {};
+  return (
+    <div>
+      <div className="border">
+        <Box
+          p={8}
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+        >
+          <img className="mb-5 w-[9rem]" src={logo} alt="" />
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+          >
+            {(formikProps) => (
+              <Form className="space-y-8">
+                <Field name="email">
+                  {({ field, form }: FieldProps<string, FormValues>) => (
+                    <FormControl
+                    // isInvalid={form?.errors?.email && form?.touched?.email}
+                    >
+                      <Input
+                        className="w-full "
+                        {...field}
+                        id="emaail"
+                        placeholder="이메일을 입력하세요"
+                      ></Input>
+                      <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+
+                <Field name="password">
+                  {({ field, form }: FieldProps<string, FormValues>) => (
+                    <FormControl
+                    // isInvalid={form.errors.password && form.touched.password}
+                    >
+                      <Input
+                        className="w-full "
+                        {...field}
+                        id="password"
+                        placeholder="이메일을 입력하세요"
+                      ></Input>
+                      <FormErrorMessage>
+                        {form.errors.password}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+
+                <Button
+                  className="w-full"
+                  mt={4}
+                  colorScheme="blue"
+                  type="submit"
+                  isLoading={formikProps.isSubmitting}
+                >
+                  로그인
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </div>
+      <div className="border w-full border-slate-300 mt-5">
+        <p className="text-center py-2">
+          계정이 없으신가요?{" "}
+          <span
+            onClick={() => navigate("/signup")}
+            className="ml-2 text-blue-700 cursor-pointer"
+          >
+            가입하기
+          </span>{" "}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
